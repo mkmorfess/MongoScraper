c3a693597f1c05fa2338965010873fe5aef05714
Michael Morfessis <Michael.Morfessis@globalinformationsystems.com>
2018-02-01 06:44:11 -0600

    Created server and some HTML/CSS for site

---
+++ server.js
@@ -0,0 +1,37 @@
+const cheerio = require('cheerio');
+const request = require('request');
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const path = require('path');
+
+// Set port
+const PORT = process.env.PORT || 3000
+
+//Init App
+const app = express();
+
+// View engine
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// Body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// Static folder
+app.use(express.static(path.join(__dirname, './public')));
+
+// require('./routes/htmlRoutes.js')(app)
+const routes = require('./routes/route.js')
+
+app.use("/", routes);
+
+app.listen(PORT, function () {
+    console.log("Server started on " + PORT);
+});
