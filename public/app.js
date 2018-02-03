102e7e98b59d5df0daa3c598279b2138a751551f
Michael Morfessis <mkmorfess@yahoo.com>
2018-02-02 18:05:37 -0600

    Fully Functioning App

--- public/app.js
+++ public/app.js
@@ -5,9 +5,88 @@ $(document).ready(function(){
 
 	$("#scraper").on("click", function(){
 
+		$("#news").empty();
+
 		$.get("/all").done(function(data){
 			console.log(data)
+			for (var i = 0; i < data.length; i++) {
+
+				var newStory = $("<div>")
+				newStory.addClass("container text-left newsList")
+				$("#news").append(newStory)
+
+				var newImage = $("<img>")
+				newImage.attr("src", data[i].image)
+				newImage.addClass("picImage")
+				newStory.append(newImage)
+
+				var newDiv = $("<h4>")
+				newDiv.append(data[i].title)
+				newStory.append(newDiv)
+
+
+				var newSummary = $("<p>")
+				newSummary.text(data[i].summary)
+				newStory.append(newSummary)
+
+				var newLink = $("<a>")
+				newLink.addClass("row")
+				newLink.attr("href", data[i].link)
+				newLink.attr("target", "_blank")
+				newLink.append("Read More Here")
+				newStory.append(newLink)
+
+				var newButton = $("<button>")
+				newButton.addClass("save btn-warning")
+				newButton.text("Save This Article")
+				newStory.append(newButton)
+			}
 		})
+
+	})
+
+	$(document).on("click", ".save", function(){
+
+		var data = {
+			saveThisHeader: $(this).siblings("h4").text(),
+			saveThisLink: $(this).siblings("a").attr("href"),
+			saveThisSummary: $(this).siblings("p").text(),
+			saveThisImage: $(this).siblings("img").attr("src")
+		}
+		
+		$.ajax({
+			url: "/save",
+			type: "POST",
+			data: data
+		}).done(function(response){
+			console.log("All done, here is your response: " + response)
+			alert("Article Saved")
+		})
+
+
+	})
+
+
+
+	$.get("/save", function(data){
+		console.log(data)
+	})
+
+
+	$(document).on("click", ".delete", function(){
+
+		var data = {
+			thisId: $(this).parent("div").attr("data-id")
+		}
+		
+		$.ajax({
+			url: "/save/" + data.thisId,
+			type: "DELETE"
+		}).done(function(response){
+			console.log("All done, here is your response: " + response)
+			location.reload();
+		})
+
 
 	})
 
--- routes/route.js
+++ routes/route.js
@@ -2,22 +2,95 @@ const express = require('express');
 const router = express.Router();
 const cheerio = require('cheerio');
 const request = require('request');
+const mongojs = require("mongojs");
+
+var databaseUrl = "mongoScraper";
+var collections = ["articles"];
+
+var db = mongojs(databaseUrl, collections);
+
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
 
 router.get('/', (req, res) => {
   res.render('index');
 });
 
 router.get('/all', (req, res) => {
-  res.json("sup"); //input your articles from New York Times in here with a cheerio web scraper...
+  request('http://www.nytimes.com/pages/todayspaper/index.html?action=Click&module=HPMiniNav&region=TopBar&WT.nav=page&contentCollection=TodaysPaper&pgtype=Homepage', function (error, response, html) {
+  if (!error && response.statusCode == 200) {
+  	var $ = cheerio.load(html);
+  	var results = [];
+  	$("div.story").children("h3").children("a").each(function(i, element) {
+  		
+
+  		var title = $(element).text();
+  		
+
+    	var link = $(element).attr("href");
+
+    	var summary = $(element).parent().parent("div.story").children("p.summary").text()
+
+    	var image = $(element).parent().parent("div.story").children(".thumbnail").children("a").children("img").attr("src")
+
+    	results.push({
+      		title: title,
+      		summary: summary,
+      		link: link,
+      		image: image
+    	});
+  	})
+    res.send(results)
+  }
+});
 });
 
 
 router.get("/save", (req, res) => {
-	res.render('save') // this route will pull all the data from mongo DB
+	db.articles.find({}).sort({_id: -1}, function(err, data){
+		var newData = {
+			saved: data
+		}
+		if (err) {
+      		console.log(err);
+    	}
+    	else {
+      		res.render('save', newData)
+    	}
+	})
+	
 })
 
 router.post('/save', (req, res) => {
-	//This is where you'll take the article and save it to mongo db and post to the saved page
+	db.articles.insert({
+		title: req.body.saveThisHeader,
+		link: req.body.saveThisLink, 
+		summary: req.body.saveThisSummary, 
+		image: req.body.saveThisImage
+	}, function(err, data){
+
+		if (err) {
+      		console.log(err);
+    	}
+    	else {
+      		res.json(data);
+    	}
+	})
+})
+
+
+router.delete('/save/:id', (req, res) => {
+	console.log(req.params.id)
+	db.articles.remove({_id: db.ObjectId(req.params.id)}, function(err, data){
+
+		if (err) {
+      		console.log(err);
+    	}
+    	else {
+      		res.json(data)
+    	}
+	})
 })
 
 module.exports = router;
